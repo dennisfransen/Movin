4dbf7192809860bcac8afed5d7b999be383908ba
dennisfransen <dennisfransen@hotmail.se>
2018-11-24 20:11:47 +0100

    Added buttons to toolbar. Added temp logo

--- app/src/main/java/dennisfransen/iths/se/movin/MainActivity.java
+++ app/src/main/java/dennisfransen/iths/se/movin/MainActivity.java
@@ -5,14 +5,34 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton mAbout, mSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAbout = findViewById(R.id.about_btn);
+        mAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "About", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mSettings = findViewById(R.id.settings_btn);
+        mSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -21,10 +41,13 @@ public class MainActivity extends AppCompatActivity {
                 switch (menuItem.getItemId()) {
                     case R.id.action_add:
                         Toast.makeText(MainActivity.this, "Action add", Toast.LENGTH_SHORT).show();
+                        break;
                     case R.id.action_edit:
                         Toast.makeText(MainActivity.this, "Action edit", Toast.LENGTH_SHORT).show();
+                        break;
                     case R.id.action_remove:
                         Toast.makeText(MainActivity.this, "Action remove", Toast.LENGTH_SHORT).show();
+                        break;
                 }
                 return true;
             }
